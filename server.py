b3e82893774751b404dbb564bedb6dff1a9819d4
Glitch (flask-demo) <none>
2023-03-18 22:37:45 +0000

    🚍🏟 Checkpoint ./server.py:55725517/623

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from datetime import datetime
 import model
-
 
 app = Flask(__name__)
 
@@ -10,6 +10,15 @@ app = Flask(__name__)
 def home():
   
   return
+
+@app.route("/api/welcome/<name>"):
+def welcome(name):
+  # API to get the welcome message from name (passed in)
+  
+  return f"<p>Hello {name}, it's a new day, <b>{datetime.weekday()}</b>. What will you do today?</p>"
+
+
+  
   
 if __name__ == "__main__":
   app.run()
