17f79aa4bb75da9b759a2d13d24485af72abf70f
Glitch (flask-demo) <none>
2023-03-18 23:24:43 +0000

    👳🚁 Checkpoint ./static/style.css:55725517/77 ./server.py:55725517/13 ./templates/base.html:55725517/30 ./app.py:55725517/620 ./templates/login.html:55725517/523

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, url_for, redirect
+from datetime import datetime
+
+app = Flask(__name__)
+
+signed_in = False
+
+@app.route("/")
+def home():
+  if not signed_in:
+    # redirect if not logged
+    return redirect(url_for("login"))
+  else:
+    pass
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+  if request.method == 'GET':
+    return render_template("login.html")
+  else:
+    return redirect(url_for("/"))
+
+# API #
+
+@app.route("/api/welcome/<name>")
+def welcome(name):
+  # API to get the welcome message from name (passed in)
+  return f"<p>Hello {name}, it's <b>{datetime.weekday()}</b>. What will you buy?</p>"
+  
+  
+if __name__ == "__main__":
+  app.run()
