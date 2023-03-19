d1170bd28213d140bd1f19a738038f507009b667
Glitch (flask-demo) <none>
2023-03-19 00:35:31 +0000

    🏇⏲ Checkpoint ./app.py:43157767/598:55725517/522

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
-sfrom flask import Flask, render_template, request, url_for, redirect, flash
+from flask import Flask, render_template, request, url_for, redirect, flash
 from datetime import datetime
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 
 app = Flask(__name__)
 
 signed_in = False
 
 @app.route("/")
-def home():
+def index():
   if not signed_in:
     # redirect if not logged
     return redirect(url_for("login"))
   else:
     return render_template("home.html")
+  
+@app.route("/home")
+def home(): 
+    return "hi"
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
   if request.method == 'GET':
     return render_template("login.html")
-  else if request.method == 'POST':
-    if request.form["email-input"] == process.env.EMAIL and request.form["password-input"] == process.env.PASSWORD:
+  elif request.method == 'POST':
+    if request.form["email-input"] == os.environ.get("EMAIL") and request.form["password-input"] == os.environ.get("PASSWORD"):
       signed_in = True
-      return redirect(url_for("home"))
+      return redirect(url_for("index"))
     else:
-      flash("Incorrect email address or password!")
-      return render_template("login.html")
+      #flash("Incorrect email address or password!")
+      return redirect(url_for("login"))
     
 # API #
 
